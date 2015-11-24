5737781e67cf64b0f0efad046b07dffe29d7d105
Christopher Brown <io@henrian.com>
2015-11-24 00:10:29 -0600

    Replace type_declarations/ directory with declarations package

--- type_declarations/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="DefinitelyTyped/async/async.d.ts" />
-/// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
-/// <reference path="DefinitelyTyped/node/node.d.ts" />
-/// <reference path="DefinitelyTyped/request/request.d.ts" />
-/// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
-
-/// <reference path="../node_modules/regex-router/regex-router.d.ts" />
-/// <reference path="../node_modules/loge/loge.d.ts" />
